f92575c89928421a1e3cfbd649f93dcad9cf8a72
adel bouras <eyyad.kareem1212@gmail.com>
2024-06-02 00:47:20 +0100

    some shit stuff

--- ln.cpp
+++ ln.cpp
@@ -2,26 +2,26 @@
 #include <string>
 #include <math.h>
 bool hasRacin(int x){
-int a=static_cast<int>(sqrt(x));
-return (a*a==x);
+int number=static_cast<int>(sqrt(x));
+return (number*number==x);
 }
 std::string intToString(int x){
-    std::string a="";
+    std::string number="";
     while(x!=0){
         switch(x%10){
-            case 1:a='1'+a; break;
-            case 2:a='2'+a; break;
-            case 3:a='3'+a; break;
-            case 4:a='4'+a; break;
-            case 5:a='5'+a; break;
-            case 6:a='6'+a; break;
-            case 7:a='7'+a; break;
-            case 8:a='8'+a; break;
-            case 9:a='8'+a; break;
+            case 1:number='1'+number; break;
+            case 2:number='2'+number; break;
+            case 3:number='3'+number; break;
+            case 4:number='4'+number; break;
+            case 5:number='5'+number; break;
+            case 6:number='6'+number; break;
+            case 7:number='7'+number; break;
+            case 8:number='8'+number; break;
+            case 9:number='8'+number; break;
         }
         x/=10;
     }
-    return a;
+    return number;
 }
   std::string ln(int x){
     if(x==1)return "0";
@@ -34,7 +34,8 @@ std::string intToString(int x){
   return(intToString(sqr)+"ln("+intToString(x)+")");
   }
 int main(){
-
-std::cout<<ln(36)<<std::endl;
+    int number;
+    std::cin>>number;
+std::cout<<ln(number)<<std::endl;
     return 0;
 }
